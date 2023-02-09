112e115de2d3ecf8169f9d124ae0c34c40298a14
Ali <ali@Alis-MacBook-Pro.local>
2023-02-09 00:01:29 -0800

    wrote reading hamlet.txt

---
+++ python-intermediate/file-io/count/count.py
@@ -0,0 +1,21 @@
+import collections
+import pathlib
+
+def count_unique_words(filename:pathlib.Path):
+    # your code here 
+    with open(filename) as f:
+        text = f.read()
+    chars = {".":"", ",":"", "!":"", "?":"", ":":"", ";":"", "\n":" ", "\'":"", "-":"", "[":"", "]":""}
+    for key, val in chars.items():
+        text = text.replace(key, val)
+    text = text.split(' ')
+    text = [word.strip().lower() for word in text if word!=""]
+    final = [f'{word} - {count}' for word, count in 
+             sorted(collections.Counter(text).items(), key=lambda x: x[1], reverse=True)]
+    return final
+
+
+if __name__ == '__main__':
+    text = count_unique_words(pathlib.Path('./hamlet.txt'))
+    for word in text[:10]:
+        print(word)
